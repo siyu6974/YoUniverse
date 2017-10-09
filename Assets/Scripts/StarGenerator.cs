48af959bfc9a69c9f9a63b4c67efcebf896b3294
Unknown <astro6974@gmail.com>
2017-10-09 16:38:47 +0200

    COLOR

--- Assets/Scripts/StarGenerator.cs
+++ Assets/Scripts/StarGenerator.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+// HIP,BayerFlamsteed,ProperName,RA,Dec,Distance,Mag,AbsMag,Spectrum,ColorIndex,X,Y,Z
 public class StarGenerator : MonoBehaviour {
     // TODO: https://www.youtube.com/watch?v=-E32N-0qwVM&t=279s
     public TextAsset starCSV;
@@ -11,15 +11,14 @@ public class StarGenerator : MonoBehaviour {
 
     public int starsMax = 100;
     public float starSize = 1;
-    public float starDistance = 10;
 
     // Use this for initialization
     void Start() {
-        CreateStars();
+        createStars();
     }
 
 
-    private void CreateStars() {
+    private void createStars() {
         starParticles = new ParticleSystem.Particle[starsMax];
         string[] lines = starCSV.text.Split('\n');
         for (int i = 0; i < starsMax; i++) {
@@ -35,11 +34,33 @@ public class StarGenerator : MonoBehaviour {
 
             //TODO: Dark stars are too dark to be visible
             //UNDONE: Add no mesh collider only prefab for raycasting
-            starParticles[i].startColor = Color.white * (1.0f - ((float.Parse(components[6]) + 1.44f) / 8)) * 2;
+            //Debug.Log(components[9]);
+
+            try {
+                //starParticles[i].startColor =Color.white * (1.0f - ((float.Parse(components[6]) + 1.44f) / 8)) *2;
+
+                starParticles[i].startColor = getColor(float.Parse(components[9])) * (1.0f - ((float.Parse(components[6]) + 1.44f) / 8)) * 3;
+            } catch (System.FormatException e) {
+                starParticles[i].startColor = Color.white * (1.0f - ((float.Parse(components[6]) + 1.44f) / 8)) * 2;
+            }
+            Debug.Log(starParticles[i].startColor);
             starParticles[i].startSize = starSize;
 
         }
         ps.SetParticles(starParticles, starParticles.Length);
+    }
+
+
+    // BV < -0.4,+2.0 >
+    // Returns Color with RGB <0,1> 
+    private Color getColor(float bv) {
+        float t, r = 0.0f, g = 0.0f, b = 0.0f;
+        if (bv < -0.4) bv = -0.4f; if (bv > 2.0) bv = 2.0f;
+            if ((bv >= -.4f) && (bv < .0f)) { t = (bv + .4f) / (.0f + .4f); r = 0.61f + (0.11f * t) + (0.1f * t * t); } else if ((bv >= .0f) && (bv < .4f)) { t = (bv - .0f) / (.4f - .0f); r = 0.83f + (0.17f * t); } else if ((bv >= .4f) && (bv < 2.10f)) { t = (bv - .4f) / (2.10f - .4f); r = 1.00f; }
+            if ((bv >= -.4f) && (bv < .0f)) { t = (bv + .4f) / (.0f + .4f); g = 0.70f + (0.07f * t) + (0.1f * t * t); } else if ((bv >= .0f) && (bv < .4f)) { t = (bv - .0f) / (.4f - .0f); g = 0.87f + (0.11f * t); } else if ((bv >= .4f) && (bv < 1.60f)) { t = (bv - .4f) / (1.60f - .4f); g = 0.98f - (0.16f * t); } else if ((bv >= 1.60f) && (bv < 2.00f)) { t = (bv - 1.60f) / (2.00f - 1.60f); g = 0.82f - (0.5f * t * t); }
+            if ((bv >= -.4f) && (bv < .4f)) { t = (bv + .4f) / (.4f + .4f); b = 1.00f; } else if ((bv >= .4f) && (bv < 1.5f)) { t = (bv - .4f) / (1.5f - .4f); b = 1.00f - (0.47f * t) + (0.1f * t * t); } else if ((bv >= 1.50f) && (bv < 1.94)) { t = (bv - 1.50f) / (1.94f - 1.50f); b = 0.63f - (0.6f * t * t); }
+        Vector3 temp = new Vector3(r, g, b).normalized;
+        return new Color(temp.x, temp.y, temp.z);
     }
 
 
