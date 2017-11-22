394b92da03d07b3f056821747b603f644cf753aa
Siyu <astro6974@gmail.com>
2017-11-22 14:22:20 +0100

    Fix zoom

--- Assets/Scripts/ZoomController.cs
+++ Assets/Scripts/ZoomController.cs
@@ -4,40 +4,40 @@ using UnityEngine;
 using UnityEngine.VR;
 
 public class ZoomController : MonoBehaviour {
-	float rate = 500.0f;
-	bool flag = false;
-	Vector3 initPos = Vector3.zero;
-	Vector3 aimedPos = Vector3.zero;
+    float rate = 500.0f;
+    bool zoomed = false;
+    Vector3 initPos = Vector3.zero;
+    Vector3 aimedPos = Vector3.zero;
 
-	public GameObject bodyPivot; // cameras
-	public StarGenerator sg;
+    public GameObject bodyPivot; // cameras
+    public StarGenerator sg;
 
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
 
         bool vrCtr = VRModeDetector.isInVR;
 
         bool vrInput = (Vector3.Distance(InputTracking.GetLocalPosition(VRNode.LeftHand), InputTracking.GetLocalPosition(VRNode.LeftEye)) < .1f);
 
-        if ((vrCtr && vrInput) || Input.GetKeyDown (KeyCode.P) && flag == false)
-		{
-			Debug.Log ("Zoom in");
-			initPos = bodyPivot.transform.position;
-			aimedPos = initPos + Camera.main.transform.forward * rate;
-			bodyPivot.transform.position = aimedPos;
-			flag = true;
-			sg.ignoreMovement = true;
-		}
-        if (((vrCtr && !vrCtr) || Input.GetKeyUp (KeyCode.P)) && flag == true)
-		{
-			Debug.Log ("Zoom out");
-			bodyPivot.transform.position = initPos;
-			flag = false;
-			sg.ignoreMovement = false;
-		}
-	}
+        if ((vrCtr && vrInput) || Input.GetKeyDown(KeyCode.P) && zoomed == false) {
+            Debug.Log("Zoom in");
+            initPos = bodyPivot.transform.position;
+            zoomed = true;
+            sg.ignoreMovement = true;
+        } else if (((vrCtr && !vrCtr) || Input.GetKeyUp(KeyCode.P)) && zoomed == true) {
+            Debug.Log("Zoom out");
+            bodyPivot.transform.position = initPos;
+            zoomed = false;
+            sg.ignoreMovement = false;
+        }
+
+        if (zoomed) {
+            aimedPos = initPos + Camera.main.transform.forward * rate;
+            bodyPivot.transform.position = aimedPos;
+        }
+    }
 }
