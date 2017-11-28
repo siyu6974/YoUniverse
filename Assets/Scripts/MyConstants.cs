30881029b7c9cf1b73099a45d796708926c37aa8
Siyu <astro6974@gmail.com>
2017-11-28 13:39:50 +0100

    Revert fake unit

--- Assets/Scripts/MyConstants.cs
+++ Assets/Scripts/MyConstants.cs
@@ -1,3 +1,3 @@
 public static class MyConstants {
-    public const float STELLAR_SYSTEM_BORDER = 1000f;  // 1AU in stellar coord
+    public const float STELLAR_SYSTEM_BORDER = 10000f;  // 1AU in stellar coord
 }
