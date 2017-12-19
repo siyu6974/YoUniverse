f9949c075b2abdcaa6b944ea88776bc3719e61d5
Siyu <astro6974@gmail.com>
2017-12-19 01:38:05 +0100

    Init warp speed

---
+++ Assets/Scripts/WarpSpeed.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WarpSpeed : MonoBehaviour {
+    public float WarpDistortion;
+    public float accelaration;
+    ParticleSystem particles;
+    ParticleSystemRenderer rend;
+    bool isWarping = true;
+
+    void Awake() {
+        particles = GetComponent<ParticleSystem>();
+        rend = particles.GetComponent<ParticleSystemRenderer>();
+    }
+
+
+    private void OnEnable() {
+        rend.velocityScale = 0.3f;
+    }
+
+
+    void Update() {
+        if (isWarping && !atWarpSpeed()) {
+            rend.velocityScale += WarpDistortion * (Time.deltaTime * accelaration);
+        }
+
+        //if(!isWarping && !atNormalSpeed())
+        //{
+        //	rend.velocityScale -= WarpDistortion * (Time.deltaTime * Speed);
+        //}
+    }
+
+    //public void Engage()
+    //{
+    //	isWarping = true;
+    //}
+
+    //public void Disengage()
+    //{
+    //	isWarping = false;
+    //}
+
+    bool atWarpSpeed() {
+        return rend.velocityScale < WarpDistortion;
+    }
+
+    bool atNormalSpeed() {
+        return rend.velocityScale > 0;
+    }
+}
